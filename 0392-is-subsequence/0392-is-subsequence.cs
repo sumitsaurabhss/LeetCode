d5b0df20fe5780188b35cdc4933f4c33ef02e092
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-02-09 22:23:45 +0530

    Time: 76 ms (72.85%), Space: 38 MB (23.44%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        int i=0, j=0;
+        if(string.IsNullOrEmpty(s))  return true;
+        else if(string.IsNullOrEmpty(t))  return false;
+        while(j<t.Length){
+            //Console.WriteLine($"{i} {j} {s[i]} {t[j]}");
+            if(s[i] == t[j])  i++;
+            if(i == s.Length)  return true;
+            j++;
+        }
+        return false;
+    }
+}
