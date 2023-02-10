1260e1ca4b0186b60f58ef093b29417851d67865
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-02-10 15:15:50 +0530

    Time: 111 ms (71.61%), Space: 46.2 MB (17.39%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cs
+++ 0724-find-pivot-index/0724-find-pivot-index.cs
@@ -1,21 +1,33 @@
-public class Solution {
+class Solution {
     public int PivotIndex(int[] nums) {
-        int size = nums.Length;
-        if(size == 1)  return 0;
-        
-        int [] leftRunningSum = new int[size];
-        leftRunningSum[0] = nums[0];
-        for(int i = 1; i < size; i++)  leftRunningSum[i] = nums[i] + leftRunningSum[i-1];
-            
-        int [] rightRunningSum = new int[size];
-        rightRunningSum[size-1] = nums[size-1];
-        for(int i = size-2; i >= 0; i--)  rightRunningSum[i] = nums[i] + rightRunningSum[i+1];
-        
-        if(rightRunningSum[1] == 0)  return 0;
-        int pivotIndex = 0;
-        for(int i = 1; i < size-1; i++)  if(leftRunningSum[i-1] == rightRunningSum[i+1])  return i;
-        if(leftRunningSum[size-2]  == 0)  return size-1;
-        
+        int sum = 0, leftsum = 0;
+        foreach (int x in nums) sum += x;
+        for (int i = 0; i < nums.Length; ++i) {
+            if (leftsum == sum - leftsum - nums[i]) return i;
+            leftsum += nums[i];
+        }
         return -1;
     }
 }
+
+// public class Solution {
+//     public int PivotIndex(int[] nums) {
+//         int size = nums.Length;
+//         if(size == 1)  return 0;
+        
+//         int [] leftRunningSum = new int[size];
+//         leftRunningSum[0] = nums[0];
+//         for(int i = 1; i < size; i++)  leftRunningSum[i] = nums[i] + leftRunningSum[i-1];
+            
+//         int [] rightRunningSum = new int[size];
+//         rightRunningSum[size-1] = nums[size-1];
+//         for(int i = size-2; i >= 0; i--)  rightRunningSum[i] = nums[i] + rightRunningSum[i+1];
+        
+//         if(rightRunningSum[1] == 0)  return 0;
+//         int pivotIndex = 0;
+//         for(int i = 1; i < size-1; i++)  if(leftRunningSum[i-1] == rightRunningSum[i+1])  return i;
+//         if(leftRunningSum[size-2]  == 0)  return size-1;
+        
+//         return -1;
+//     }
+// }
