0ec5926ec4d7b6577d54d936bd49da03c7787438
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-02-10 17:28:18 +0530

    Time: 213 ms (47.69%), Space: 53.5 MB (13.19%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -1,7 +1,21 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        Array.Sort(nums);
-        for(int i = 1; i < nums.Length; i++)  if(nums[i] == nums[i-1])  return true;
+
+        HashSet<int> distinct = new HashSet<int>();
+        foreach(int num in nums) {
+            if(distinct.Contains(num)) {
+                return true;
+            }
+            distinct.Add(num);
+        }
         return false;
     }
 }
+
+// public class Solution {
+//     public bool ContainsDuplicate(int[] nums) {
+//         Array.Sort(nums);
+//         for(int i = 1; i < nums.Length; i++)  if(nums[i] == nums[i-1])  return true;
+//         return false;
+//     }
+// }
