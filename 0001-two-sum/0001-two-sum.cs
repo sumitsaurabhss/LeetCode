68d7fac80915961e7b8d8f0aecd27dc65583504a
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-02-11 14:44:16 +0530

    Time: 141 ms (95.44%), Space: 44.9 MB (9.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,6 +1,24 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        int[] r = new int[2]{0,0};
+        Dictionary<int, int> seen = new Dictionary<int, int>();
+        for(int i = 0; i < nums.Length; i++){
+            //if we've seen the matching number to our number
+            if(seen.ContainsKey(target-nums[i])){
+                //then return the matching numbers index and our own
+                return new int[]{seen[target-nums[i]], i};
+            }
+            //otherwise add our value to the dictionary and continue
+            //if we've already seen this value we can ignore it since both indexes would be valid.
+            if(!seen.ContainsKey(nums[i])){
+                seen.Add(nums[i], i);
+            }
+            
+        }
+        return null;
+        
+        
+        
+        /*int[] r = new int[2]{0,0};
         for(int i = 0; i < nums.Length; i++){
             for(int j = i+1; j < nums.Length; j++){
                 if(nums[i] + nums[j] == target){
@@ -9,6 +27,6 @@ public class Solution {
                 }
             }
         }
-        return r;
+        return r;*/
     }
 }
