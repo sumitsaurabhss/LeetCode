d9fc61c9a64f28d9b8aa87178e54d021be4f305a
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-02-11 13:29:54 +0530

    Time: 13 ms (98.83%), Space: 23.9 MB (18.07%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cs
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cs
@@ -1,14 +1,14 @@
 public class Solution {
     public int HammingWeight(uint n) {
         int count = 0;
-        /*string s = n.ToString();
-        Console.WriteLine(s);
-        foreach(char c in s)  if(c == '1')  count++;*/
+        string s = Convert.ToString(n, 2);
+        //Console.WriteLine(s);
+        foreach(char c in s)  if(c == '1')  count++;
         
-        while(n > 0){
+       /* while(n > 0){
             if(n % 2 == 1)  count++;
             n /= 2;
-        }
+        }*/
         
         return count;
     }
