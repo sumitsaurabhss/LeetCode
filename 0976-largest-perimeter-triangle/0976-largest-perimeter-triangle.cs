5b1a09cb5409da0c5f73eb4e7602000ac9c46551
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-02-12 10:43:51 +0530

    Time: 126 ms (59.33%), Space: 47 MB (25.33%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public int LargestPerimeter(int[] nums) {
+        Array.Sort(nums);
+        for(int i=nums.Length-1; i>1; i--)  if(nums[i] < nums[i-1]+nums[i-2])  return nums[i]+nums[i-1]+nums[i-2];
+        return 0;
+    }
+}
