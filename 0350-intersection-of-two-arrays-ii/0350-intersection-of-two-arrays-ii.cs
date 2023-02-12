0d1000cb39d8e780a31690706973e20ff54fe222
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-02-12 13:53:50 +0530

    Time: 153 ms (38.68%), Space: 43.4 MB (17.75%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cs
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cs
@@ -1,18 +1,30 @@
 public class Solution {
     public int[] Intersect(int[] nums1, int[] nums2) {
-        Array.Sort(nums1);
-        Array.Sort(nums2);
         List<int> l = new List<int>();
-        int i=0, j=0;
-        while(i<nums1.Length && j<nums2.Length){
-            if(nums1[i] < nums2[j])  i++;
-            else if(nums1[i] > nums2[j])  j++;
-            else  {
-                l.Add(nums1[i]);
-                i++;
-                j++;
+        Dictionary<int, int> d = new Dictionary<int, int>();
+        for(int i = 0; i < nums1.Length; i++){
+            if(d.ContainsKey(nums1[i]))  d[nums1[i]]++;
+            else  d.Add(nums1[i], 1);
+        }
+        for(int i = 0; i < nums2.Length; i++){
+            if(d.ContainsKey(nums2[i])){
+                l.Add(nums2[i]);
+                d[nums2[i]]--;
+                if(d[nums2[i]] == 0)  d.Remove(nums2[i]);
             }
         }
+        // Array.Sort(nums1);
+        // Array.Sort(nums2);
+        // int i=0, j=0;
+        // while(i<nums1.Length && j<nums2.Length){
+        //     if(nums1[i] < nums2[j])  i++;
+        //     else if(nums1[i] > nums2[j])  j++;
+        //     else  {
+        //         l.Add(nums1[i]);
+        //         i++;
+        //         j++;
+        //     }
+        // }
         return l.ToArray();
     }
 }
