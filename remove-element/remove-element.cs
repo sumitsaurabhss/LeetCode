eaf661f55f5195fab02349a7773e5585feec7569
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-02-20 19:26:53 +0530

    Time: 153 ms, Memory: 42.8 MB - LeetHub

---
+++ remove-element/remove-element.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int n = nums.Length;
+        int i = 0, k = n-1;
+        while(i <= k){
+            if(nums[i] == val){
+                (nums[i], nums[k]) = (nums[k], nums[i]);
+                k--;
+                Console.WriteLine(i + " " + k);
+            }
+            else  i++;
+        }
+        if(i-k >= 2)  return --i;
+        Console.WriteLine(i + " " + k);
+        return i;
+    }
+}
