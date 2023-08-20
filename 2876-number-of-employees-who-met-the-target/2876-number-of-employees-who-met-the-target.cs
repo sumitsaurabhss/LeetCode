bcfb08b7d327b9a15a28af0415332f2cb1c27497
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-08-20 23:15:28 +0530

    Time: 80 ms (60.25%), Space: 40.7 MB (9.78%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.cs
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.cs
@@ -1,10 +1,11 @@
 public class Solution {
     public int NumberOfEmployeesWhoMetTarget(int[] hours, int target) {
-        int counter = 0;
-        foreach(int hour in hours) {
-            if(hour >= target)
-                counter++;
-        }
-        return counter;
+        // int counter = 0;
+        // foreach(int hour in hours) {
+        //     if(hour >= target)
+        //         counter++;
+        // }
+        // return counter;
+        return hours.Where(s => s >= target).Count();
     }
 }
