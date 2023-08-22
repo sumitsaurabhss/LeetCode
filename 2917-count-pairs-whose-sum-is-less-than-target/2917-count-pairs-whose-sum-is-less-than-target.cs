fbb24eda9e88d22b7f361cdec7ba4536a3798f84
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-08-22 16:31:40 +0530

    Time: 86 ms (66.25%), Space: 40.9 MB (27.5%) - LeetHub

--- 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.cs
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.cs
@@ -1,19 +1,25 @@
 public class Solution {
     public int CountPairs(IList<int> nums, int target) {
-        int[] a = nums.ToArray();
-        Array.Sort(a);
-        //Console.Write(String.Join(", ", a));
-        int counter = 0;
-        int l = 0, r = a.Length - 1;
-        while(l < r) {
-            if(a[l] + a[r] < target) {
-                counter += r - l;
-                l++;
-            }
-            else {
-                r--;
-            }
-        }
-        return counter;
+        // int[] a = nums.ToArray();
+        // Array.Sort(a);
+        // //Console.Write(String.Join(", ", a));
+        // int counter = 0;
+        // int l = 0, r = a.Length - 1;
+        // while(l < r) {
+        //     if(a[l] + a[r] < target) {
+        //         counter += r - l;
+        //         l++;
+        //     }
+        //     else {
+        //         r--;
+        //     }
+        // }
+        // return counter;
+        int ans = 0;
+        for (int i = 0; i < nums.Count - 1; i++)
+            for (int j = i + 1; j < nums.Count; j++)
+                if (nums[i] + nums[j] < target)
+                    ans++;
+        return ans;
     }
 }
