bc7949fe8afd599f041e8087b338f9a37aa1f4fb
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-08-22 14:22:15 +0530

    Time: 84 ms (83.26%), Space: 36.3 MB (27.68%) - LeetHub

---
+++ 2556-convert-the-temperature/2556-convert-the-temperature.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public double[] ConvertTemperature(double celsius) {
+        double[] a = new double[2];
+        a[0] = celsius + 273.15;
+        a[1] = celsius * 1.80 + 32.00;
+        return a;
+    }
+}
