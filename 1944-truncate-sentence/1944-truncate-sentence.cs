bc54f505a74d72594b8b65e9f2b23ab0de389eb5
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-09-06 17:38:17 +0530

    Time: 86 ms (15.18%), Space: 37.3 MB (76.79%) - LeetHub

--- 1944-truncate-sentence/1944-truncate-sentence.cs
+++ 1944-truncate-sentence/1944-truncate-sentence.cs
@@ -1,14 +1,23 @@
 public class Solution {
     public string TruncateSentence(string s, int k) {
-        int w = 0;
-        StringBuilder sb = new StringBuilder();
-        foreach(char c in s) {
-            if(c == ' ') {
+        // int w = 0;
+        // StringBuilder sb = new StringBuilder();
+        // foreach(char c in s) {
+        //     if(c == ' ') {
+        //         w++;
+        //         if(w == k)  break;
+        //     }
+        //     sb.Append(c);
+        // }
+        // return sb.ToString();
+        int i = 0, w = 0, l = s.Length;
+        while(i < l) {
+            if(s[i] == ' ') {
                 w++;
-                if(w == k)  break;
+                if(w == k)  return s.Substring(0, i);
             }
-            sb.Append(c);
+            i++;
         }
-        return sb.ToString();
+        return s;
     }
 }
