54d36812b0f7889bcea41e2a8f5790be37e8b0b6
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-09-07 09:16:17 +0530

    Time: 89 ms (80.08%), Space: 42.4 MB (36.1%) - LeetHub

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.cs
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.cs
@@ -1,12 +1,11 @@
 public class Solution {
     public int DifferenceOfSum(int[] nums) {
         int esum = 0, dsum = 0;
-        foreach(int n in nums) {
-            esum += n;
-            int t = n;
-            while(t > 0) {
-                dsum += t % 10;
-                t /= 10;
+        for(int i = 0; i < nums.Length; i++) {
+            esum += nums[i];
+            while(nums[i] > 0) {
+                dsum += nums[i] % 10;
+                nums[i] /= 10;
             }
         }
         return Math.Abs(esum - dsum);
