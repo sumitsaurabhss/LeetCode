2399b0c1cd93b62dede9478ec43e5227c0d92200
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-09-08 09:00:54 +0530

    Time: 25 ms (63.92%), Space: 26.9 MB (17.01%) - LeetHub

--- 2752-sum-multiples/2752-sum-multiples.cs
+++ 2752-sum-multiples/2752-sum-multiples.cs
@@ -1,10 +1,16 @@
 public class Solution {
     public int SumOfMultiples(int n) {
         int sum = 0;
-        for(int i = 3; i <= n; i++) {
-            if(i % 3 == 0)  sum += i;
-            else if(i % 5 == 0)  sum += i;
-            else if(i % 7 == 0)  sum += i;
+        // for(int i = 3; i <= n; i++) {
+        //     if(i % 3 == 0)  sum += i;
+        //     else if(i % 5 == 0)  sum += i;
+        //     else if(i % 7 == 0)  sum += i;
+        // }
+        while(n > 2) {
+            if(n % 3 == 0)  sum += n;
+            else if(n % 5 == 0)  sum += n;
+            else if(n % 7 == 0)  sum += n;
+            n--;
         }
         return sum;
     }
