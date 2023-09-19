96220b729478cdca04a6b39368f552a6e2412071
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-09-19 06:37:09 +0530

    Time: 167 ms (91.71%), Space: 48.6 MB (97.24%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int FindDuplicate(int[] nums) {
+        int len = nums.Length;
+        int[] cnt = new int[len + 1];
+        for (int i = 0; i < len; i++) {
+            cnt[nums[i]]++;
+            if (cnt[nums[i]] > 1) {
+                return nums[i];
+            }
+        }
+
+        return len;
+    }
+}
