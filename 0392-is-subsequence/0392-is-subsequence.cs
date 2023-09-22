b5fe801165360309c187206f6c08d50067b8802a
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-09-22 09:10:57 +0530

    Time: 61 ms (82.82%), Space: 38 MB (36.35%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cs
+++ 0392-is-subsequence/0392-is-subsequence.cs
@@ -1,13 +1,8 @@
 public class Solution {
     public bool IsSubsequence(string s, string t) {
-        int i=0, j=0;
-         if(string.IsNullOrEmpty(s))  return true;
-         else if(string.IsNullOrEmpty(t))  return false;
-        while(j<t.Length){
-            //Console.WriteLine($"{i} {j} {s[i]} {t[j]}");
-            if(s[i] == t[j])  i++;
-            if(i == s.Length)  return true;
-            j++;
+        int i = 0;
+        foreach(char c in t) {
+            if(i < s.Length && s[i] == c) i++;
         }
         return i == s.Length;
     }
