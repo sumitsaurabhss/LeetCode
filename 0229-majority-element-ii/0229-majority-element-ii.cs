cc06c45815ff4fb76b49990bac410c170a422bac
Sumit Saurabh <sumitsaurabh98@gmail.com>
2023-10-05 12:53:01 +0530

    Time: 143 ms (54.27%), Space: 46.2 MB (82.48%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cs
+++ 0229-majority-element-ii/0229-majority-element-ii.cs
@@ -1,17 +1,36 @@
+// Using sort method
+// public class Solution {
+//     public IList<int> MajorityElement(int[] nums) {
+//         int n = nums.Length;
+//         int t = n / 3, c = 1;
+//         Array.Sort(nums);
+//         IList<int> list = new List<int>();
+//         for(int i = 1; i < n; i++) {
+//             if(nums[i] == nums[i-1])  c++;
+//             else {
+//                 if(c > t)  list.Add(nums[i-1]);
+//                 c = 1;
+//             }
+//         }
+//         if(c > t)  list.Add(nums[n-1]);
+//         return list;
+//     }
+// }
+
+// Using HashSet method
 public class Solution {
     public IList<int> MajorityElement(int[] nums) {
         int n = nums.Length;
-        int t = n / 3, c = 1;
-        Array.Sort(nums);
-        IList<int> l = new List<int>();
-        for(int i = 1; i < n; i++) {
-            if(nums[i] == nums[i-1])  c++;
-            else {
-                if(c > t)  l.Add(nums[i-1]);
-                c = 1;
-            }
+        int t = n / 3;
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+        IList<int> list = new List<int>();
+        foreach(int num in nums) {
+            if(dictionary.ContainsKey(num))  dictionary[num]++;
+            else  dictionary.Add(num, 1);
         }
-        if(c > t)  l.Add(nums[n-1]);
-        return l;
+        foreach(var kvp in dictionary) {
+            if(kvp.Value > t)  list.Add(kvp.Key);
+        }
+        return list;
     }
 }
